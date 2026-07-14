b7cff580373eba979360ea1abc59c4236a951a0a
Dulaksha117 <dulaksha.rasanjana@onedatasoftware.com>
2026-07-15 00:43:06 +0530

    feat: implement shopping cart module
    ### Added
    - Shopping cart domain entities
    - Cart repository implementation
    - Cart service with business logic
    - Cart controller endpoints
    - Cart DTOs
    - EF Core cart configurations
    
    ### Database
    - Added Cart and CartItem entities
    - Applied AddCartAndCartItems migration
    
    ### Features
    - Get cart
    - Add product to cart
    - Update cart item quantity
    - Remove cart item
    - Clear cart
    
    ### Improvements
    - Integrated cart into clean architecture
    - Dependency injection registrations
    - Repository and service layers

--- server/FreshMarket.API/Program.cs
+++ server/FreshMarket.API/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductImageRepository, ProductImageRepository>();
+builder.Services.AddScoped<ICartRepository, CartRepository>();
 
 // Services
 builder.Services.AddScoped<IJwtService, JwtService>();
@@ -30,6 +31,8 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();  
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<ICartService, CartService>();
+
 
 
 
---
+++ server/FreshMarket.Application/Interfaces/ICartRepository.cs
@@ -0,0 +1,24 @@
+using FreshMarket.Domain.Entities;
+
+namespace FreshMarket.Application.Interfaces;
+
+public interface ICartRepository
+{
+    Task<Cart?> GetByUserIdAsync(Guid userId);
+
+    Task<CartItem?> GetCartItemByIdAsync(Guid cartItemId);
+
+    Task AddAsync(Cart cart);
+
+    Task AddCartItemAsync(CartItem cartItem);
+
+    void Update(Cart cart);
+
+    void Delete(Cart cart);
+
+    void DeleteCartItem(CartItem cartItem);
+
+    Task SaveChangesAsync();
+
+    Task UpdateCartItemAsync(CartItem cartItem);
+}
